4151b1d214fffefb7b5ea14397ef5804f68fb787
Dave Irvine <dave@daveirvine.me>
2020-11-30 11:40:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const core = require('@actions/core');
+const aws = require('aws-sdk');
+
+async function run() {
+    try {
+        const ecs = new aws.ECS({
+            customUserAgent: 'amazon-ecs-stop-task-for-github-actions'
+        });
+
+        // Get inputs
+        const task = core.getInput('task-arn', { required: true });
+        const cluster = core.getInput('cluster', { required: false });
+        const reason = core.getInput('reason', { required: false });
+
+        try {
+            const params = {
+                task,
+                cluster,
+                reason
+            };
+
+            await ecs.stopTask(params).promise();
+        } catch (error) {
+            core.setFailed("Failed to run task in ECS: " + error.message);
+            throw (error);
+        }
+    }
+    catch (error) {
+        core.setFailed(error.message);
+        core.debug(error.stack);
+    }
+}
+
+module.exports = run;
+
+/* istanbul ignore next */
+if (require.main === module) {
+    run();
+}
